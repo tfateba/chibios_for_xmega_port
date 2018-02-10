ad46ffc842e8ef30f403fa30946b9c4c108a4872
Theodore Ateba <tf.ateba@gmail.com>
2018-02-10 08:24:58 +0100

    Correct the author name.

--- os/hal/ports/AVR/XMEGA/LLD/USARTv1/hal_serial_lld.c
+++ os/hal/ports/AVR/XMEGA/LLD/USARTv1/hal_serial_lld.c
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2016..2018 Giovanni Theodore Ateba
+    ChibiOS - Copyright (C) 2016..2018 Theodore Ateba
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
